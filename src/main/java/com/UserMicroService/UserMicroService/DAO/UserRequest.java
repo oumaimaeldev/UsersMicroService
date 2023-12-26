369e1901d2d262376b711ed68265b27af990475b
oumaima EL HACHIMI <oumaimaelhachimi55@gmail.com>
2023-12-26 12:20:04 +0000

    the Regex done 🚀

--- src/main/java/com/UserMicroService/UserMicroService/DAO/UserRequest.java
+++ src/main/java/com/UserMicroService/UserMicroService/DAO/UserRequest.java
@@ -3,7 +3,9 @@ package com.UserMicroService.UserMicroService.DAO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Data
@@ -12,18 +14,21 @@ import lombok.*;
 @AllArgsConstructor
 public class UserRequest {
     @NotBlank(message="First name cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name should contain only letters")
     private String Firstname;
     @NotBlank(message="Last name cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name should contain only letters")
     private String Lastname;
     @NotBlank(message="Address cannot be empty")
     private String Address;
     @NotBlank(message="please select role")
     private String Role;
     @NotBlank(message="Email cannot be empty")
+    @Email
     private String Email;
     @NotBlank(message="Password cannot be empty")
     private String Password;
-    @NotBlank(message="Phone cannot be empty")
+    @Pattern(regexp = "^\\+212\\d{9}$",message = "Invalid phone number")
     private String Phone;
     @NotBlank(message="please select City")
     private String City;
